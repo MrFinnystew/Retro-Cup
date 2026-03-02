61547c2e34bcb20182f2d5d199251948d72d512e
Yu-El Cheong <yuel.cheong@gmail.com>
2026-03-01 16:36:55 -0800

    Create Color Text.hpp

---
+++ Color Text.hpp
@@ -0,0 +1,31 @@
+//
+//  Color Text.hpp
+//  Retro Cup
+//
+//  Created by Yu-El Cheong on 3/1/26.
+//
+#include <iostream>
+using
+std::cout,
+std::string;
+
+string
+
+startText = "\033[",
+endText = "m",
+resetText = "\033[0m",
+
+boldText = ";1",
+italicsText = ";3",
+blueText = ";34",
+redText = ";31",
+greenText = ";32",
+yellowText = ";33",
+
+
+startBlueText = "34",
+startItalicsText = "3",
+startRedText = "31",
+startGreenText = "32",
+startYellowText = "33",
+startBoldText = "1";
